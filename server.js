0caa3b5a8a006c9720bc0478975d7a3219a93f72
Gayatri Vaka <gvaka@uncc.edu>
2020-10-01 17:57:05 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,29 +4,25 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Grocery',
-        budget: 110
-    }
-]
-};
+
+const budget = './myBudget.json';
+const fs= require("fs");
+
+
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
-})
+    //res.json(budget);
+    fs.readFile(budget,"utf8",(err,data)=>{
+        if(err){
+            throw err;
+        }
+        res.send(JSON.parse(data));
+    }
+    );
+}) 
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
